235ad5904349457977ab73fa8113b3c8d0154b8a
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2026-06-25 12:03:02 +0530

    init commit

--- Assignment07(Loop)/a13.py
+++ Assignment07(Loop)/a13.py
@@ -20,11 +20,28 @@ Output: Both numbers are same
 
 n1,n2=map(int,input("Enter The Two Number Sperated By Comma : ").split(","))
 
+# by for loops
+'''
 if n1<n2:
     for i in range(n1,n2+1):
         print(i,end=" ")
 elif n1>n2:
     for i in range(n1,n2-1,-1):
         print(i,end=" ")
+else:
+    print("both the numbers same") 
+ '''
+    
+# by while loops
+if n1<n2:
+    while n1<n2:
+        print(n1,end="->")
+        n1+=1
+    print(n2)
+elif n1>n2:
+    while n2<n1:
+        print(n1,end="->")
+        n1-=1
+    print(n1)
 else:
     print("both the numbers same") 
