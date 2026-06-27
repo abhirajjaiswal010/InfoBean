72415d02c3f8e3f8656a2f37f6120c05dcab960f
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2026-06-27 23:39:55 +0530

    assignment 09  done {27 June}

---
+++ Assignment10(loop3)/a07.py
@@ -0,0 +1,34 @@
+'''
+Docstring for a07
+ Duck Number Checker
+
+A verification system is used by an e-commerce company to validate promotional coupon numbers. Coupon numbers containing at least one zero in between digits are considered special duck numbers. However, if the number starts with zero, it is rejected immediately.
+
+A duck number is a number that contains at least one zero but does not start with zero.
+
+Example:
+1023
+
+Write a program using loops to check whether the entered number is a Duck number.
+
+Input:
+1023
+
+Output:
+Duck Number
+'''
+n=int(input("Enter The Number: "))
+flag=False
+
+while n>0:
+    d=n%10
+    if d==0:
+        flag=True
+        break
+    n//=10
+
+if flag:
+    print("duck number")
+else:
+    print("not duck number")
+    
