d88ca6f945b88ce29d2896cdba16f58079e6e6ae
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2026-07-10 09:13:15 +0530

    init commit

---
+++ Assignment17(Pattern-II)/a65.py
@@ -0,0 +1,24 @@
+n=int(input("The Number : "))
+
+i=1
+while i<=n:
+    if i<=2:
+        j=1
+        while j<=i:
+            print(1,end=" ")
+            j+=1
+        
+    else:
+        j=1
+        print(1,end=" ")
+        while j<=i-2:
+            print(i-1,end=" ")
+            j+=1
+        print(1,end=" ")
+    print()
+    i+=1
+
+
+
+    
+
