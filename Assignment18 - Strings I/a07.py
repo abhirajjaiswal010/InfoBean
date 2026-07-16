d1064588fe290739c9999129eeee1e6c6976d18e
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2026-07-17 00:20:37 +0530

    init commit

--- Assignment18 - Strings I/a07.py
+++ Assignment18 - Strings I/a07.py
@@ -15,15 +15,44 @@ Enter vehicle number: MP04AB1234
 Output:
 Valid Vehicle Number
 '''
-n=input("Enter Vehicle Number : ")
-if len(n)==10:
-    a=n[:2]
-    b=n[2:4]
-    if not a.isalpha():
-        print("Not The Alphabets first 2 ")
-    if not b.isdigit():
-        print("Not the digit ")
-    else:
-        print("Valid Vehicle Number")
+n=input("Enter Vehicle Number : ").lower()
+# print(len(n))
+# print(n.count(" "))
+# print( n[:2], n[2:4],n[4:6],n[6:])
+# if  n.count(" ")==0 and len(n)==10:
+#     a=n[:2]
+#     b=n[2:4]
+#     c=n[4:6]
+#     d=n[6:]
+    
+#     if  a.isalpha() and c.isalpha and b.isdigit() and d.isdigit():
+#         print(" valid")
+    
+#     else:
+#         print("not valid")
+# else:
+#     print("Not valid  ")
+
+if len(n)==10 and " " not in n:
+    
+          
+        if ('a' <= n[0] <= 'z' and
+            'a' <= n[1] <= 'z' and
+            '0' <= n[2] <= '9' and
+            '0' <= n[3] <= '9' and
+            'a' <= n[4] <= 'z' and
+            'a' <= n[5] <= 'z' and
+            '0' <= n[6] <= '9' and
+            '0' <= n[7] <= '9' and
+            '0' <= n[8] <= '9' and
+            '0' <= n[9] <= '9' ):
+            print("Valid")
+        else:
+            print("Invalid")
+            
 else:
-    print("Not leagth 10 ")
+    print("invalid")
+
+
+
+
