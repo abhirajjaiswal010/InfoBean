e89a8d1a6083fb554086ccaefa35ab558392e709
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2026-09-08 13:58:02 +0530

    init commit

---
+++ Assignment37 - Recursion I/10_MobileRechargePrimeCheck.py
@@ -0,0 +1,18 @@
+'''
+Assignment 10: Mobile Recharge System
+
+A telecom company issues lucky recharge coupons only if the coupon number is prime.
+
+Task:
+Write a recursive function to determine whether a given number is prime.
+
+Input:
+Enter Coupon Number:
+29
+
+Output:
+Prime Number
+'''
+prime=False
+def check_prime(n):
+    
