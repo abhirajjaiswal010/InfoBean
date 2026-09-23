4585e7fc03f89c124120eb683f4c9a0e29f6ca22
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2026-09-23 23:08:10 +0530

    init commit

--- Assignment18 - Strings I/a02.py
+++ Assignment18 - Strings I/a02.py
@@ -9,7 +9,7 @@ Input: Enter chat message: Good morning everyone how are you
 Output: Total spaces: 5
 '''
 
-n=input("Enter : ")
+n=input("Enter : ").strip()
 count=0
 i=0
 while i <len(n):
@@ -19,3 +19,18 @@ while i <len(n):
 
 
 print(count)
+
+
+n = input("Enter: ")
+
+count = 0
+i = 0
+
+while i < len(n):
+    if n[i] == " ":
+        if i != 0 and i != len(n) - 1:
+            # Agar current character space hai AND woh first ya last character nahi hai, tab count karo.
+            count += 1
+    i += 1
+
+print("Total spaces:", count)
